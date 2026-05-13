92e9cc4819fd31fc8ee42ad99e8c26a92d859add
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2026-05-13 23:47:42 +0530

    gg

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3000;  
+const products = require('./data.json');
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Product API');
+});
+
+
+
+// Get Electronics Items
+app.get('/electronics', (req, res) => {
+  const electronicsItems = products.filter(
+    (item) => item.mainCategory === "Electronics"
+  );
+
+  res.json(electronicsItems);
+});
+
+
+
+// Get Fashion Items
+app.get('/fashion', (req, res) => {
+  const fashionItems = products.filter(
+    (item) => item.mainCategory === "Fashion"
+  );
+
+  res.json(fashionItems);
+});
+
+
+
+// Get Household Items
+app.get('/household', (req, res) => {
+  const householdItems = products.filter(
+    (item) => item.mainCategory === "House"
+  );
+
+  res.json(householdItems);
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
