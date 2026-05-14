40c18c105090fbc33b7344fb70626ad7fd6dc249
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2026-05-14 07:31:08 +0530

    gg

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;  
 const products = require('./data.json');
+const cors = require('cors');
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Product API');
